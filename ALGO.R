22515952fe04a18ee8f7925e3a21f80af73db8a2
philschus <59765557+philschus@users.noreply.github.com>
2024-10-01 22:14:33 +0200

    consistent formatting

--- ALGO.R
+++ ALGO.R
@@ -1,215 +1,214 @@
 # computes demographic transition (and updates intervivo transfers accordingly)
-compdemo <- function() {
-  
+compdemo = function() {
+
   # compute demography transition
   for (tt in 2:tend) {
-    Nv[1,tt]  <<- NB[tt];
+    Nv[1, tt]  <<- NB[tt]
     for (i in 2:nag) {
-      Nv[i,tt] <<- Nv[i-1,tt-1]*gamv[i-1,tt-1];
+      Nv[i, tt] <<- Nv[i - 1, tt - 1] * gamv[i - 1, tt - 1]
     }
   }
-  
-  Nz   <<- per2coh(Nv);
-  N    <<- aggcoh2per(Nz);
-  Nc[] <<- colSums(Nv[1:(fag-1),]);
-  
+
+  Nz   <<- per2coh(Nv)
+  N    <<- aggcoh2per(Nz)
+  Nc[] <<- colSums(Nv[1:(fag - 1), ])
+
   # Compute neutral intervivo-transfers by rescaling received transfers
   for (tt in 1:tend) {
-    ivgiven          = -sum(Nv[,tt]*ivv[,tt]*(ivv[,tt]<0));
-    ivreceived       = sum(Nv[,tt]*ivv[,tt]*(ivv[,tt]>0));
-    
-    ivv[ivv[,tt]>0,tt] <<- ivv[ivv[,tt]>0,tt]*(ivgiven/ivreceived);
-    
-    if (abs(sum(ivv[,tt]*Nv[,tt]))>1e-10) cat("ERROR IN RECOMPDEMO: Unbalanced intervivo transfers!");
-    
+    ivgiven          = -sum(Nv[, tt] * ivv[, tt] * (ivv[, tt] < 0))
+    ivreceived       = sum(Nv[, tt] * ivv[, tt] * (ivv[, tt] > 0))
+
+    ivv[ivv[, tt] > 0, tt] <<- ivv[ivv[, tt] > 0, tt] * (ivgiven / ivreceived)
+
+    if (abs(sum(ivv[, tt] * Nv[, tt])) > 1e-10) cat("ERROR IN RECOMPDEMO: Unbalanced intervivo transfers!")
+
   }
-  
-  ivz <<- per2coh(ivv);
+
+  ivz <<- per2coh(ivv)
 
 }
 
 # computes the further life expectancy
-lifeexpect <- function(gamv) {
-  
-  nag = length(gamv);
-  
-  lifeexpectageN = zeroscol(nag);
-  for (a in (nag-1):1) {
-    lifeexpectageN[a] = (lifeexpectageN[a+1]+1)*gamv[a]+gamv[a+1]*(1-gamv[a]);
+lifeexpect = function(gamv) {
+
+  nag = length(gamv)
+
+  lifeexpectageN = zeroscol(nag)
+  for (a in (nag - 1):1) {
+    lifeexpectageN[a] = (lifeexpectageN[a + 1] + 1) * gamv[a] + gamv[a + 1] * (1 - gamv[a])
   }
-  
-  return(lifeexpectageN);
+
+  return(lifeexpectageN)
 }
 
 # main routine that solves the transition path of the full model
-solveOLG_GFT <- function(starttime = 1, maxiter = 200L, tol = 1e-5, tune0 = 0.9, tune1 = 0.9) {
+solveOLG_GFT = function(starttime = 1, maxiter = 200L, tol = 1e-5, tune0 = 0.9, tune1 = 0.9) {
 
-  #===== demography ======#
-  compdemo(); # recomputes demographic transition
-  
-  #===== final steady state =====#
-  FSS();
-  
+  # ===== demography ======#
+  compdemo() # recomputes demographic transition
+
+  # ===== final steady state =====#
+  FSS()
+
   actual = gft(PATH, guess, starttime, maxtry = maxiter, tol = tol, tune0 = tune0, tune1 = tune1, iprint = 1L)
-  
-  # convert period-view variables to cohort-view variables
-  Az       <<- per2coh(Av);
-  Consz    <<- per2coh(Consv);
-  Hz       <<- per2coh(Hv);
-  Lambdaz  <<- per2coh(Lambdav);
-  Qz       <<- per2coh(Qv);
-  Savz     <<- per2coh(Savv);
-  dis_totz <<- per2coh(dis_totv);
-  ellz     <<- per2coh(ellv);
-  rz       <<- per2coh(rv);
-  wz       <<- per2coh(wv);
-  pcz      <<- per2coh(pcv);
-  yz       <<- per2coh(yv);
 
-  cat(paste0("CHECK SOLUTION:\t\t", max(abs(edy)+abs(edl)+abs(edg)+abs(eda)+abs(ediv)+abs(edab))),"\n");
-    
+  # convert period-view variables to cohort-view variables
+  Az       <<- per2coh(Av)
+  Consz    <<- per2coh(Consv)
+  Hz       <<- per2coh(Hv)
+  Lambdaz  <<- per2coh(Lambdav)
+  Qz       <<- per2coh(Qv)
+  Savz     <<- per2coh(Savv)
+  dis_totz <<- per2coh(dis_totv)
+  ellz     <<- per2coh(ellv)
+  rz       <<- per2coh(rv)
+  wz       <<- per2coh(wv)
+  pcz      <<- per2coh(pcv)
+  yz       <<- per2coh(yv)
+
+  cat(paste0("CHECK SOLUTION:\t\t", max(abs(edy) + abs(edl) + abs(edg) + abs(eda) + abs(ediv) + abs(edab))), "\n")
+
 }
 
 # computes a path of temporary equilibria for given guess of foresigh variables
-PATH <- function(guess, tstart = 1, tstop = (tend-1)) {
-    
-  V		              <<- t(guess[,1]);
-  Hv[fag:nag,]      <<- t(guess[,(1:(nag-fag+1))+1]);
-  Lambdav[fag:nag,] <<- t(guess[,(nag-fag+3):numforesight]);
-  
-  tt		            <<- tstart;
-  
-  xTEPATH0          = onescol(4);
-  
+PATH = function(guess, tstart = 1, tstop = (tend - 1)) {
+
+  V                           <<- t(guess[, 1])
+  Hv[fag:nag, ]      <<- t(guess[, (1:(nag - fag + 1)) + 1])
+  Lambdav[fag:nag, ] <<- t(guess[, (nag - fag + 3):numforesight])
+
+  tt                        <<- tstart
+
+  xTEPATH0          = onescol(4)
+
   # TEMPORARY EQUILIBRIA
   while (tt <= tstop) {
-    
+
     if (tt > 1) {
-      
-      xTEPATH0[1] = r[tt-1]/r0;
-      xTEPATH0[2] = w[tt-1]/w0;
-      
+
+      xTEPATH0[1] = r[tt - 1] / r0
+      xTEPATH0[2] = w[tt - 1] / w0
+
       # budget-instrument choice
-      if (budget_bal == 1) xTEPATH0[3] = 1+tauW[tt-1]-tauW0;
-      if (budget_bal == 2) xTEPATH0[3] = 1+tauF[tt-1]-tauF0;
-      if (budget_bal == 3) xTEPATH0[3] = 1+tauC[tt-1]-tauC0;
-      if (budget_bal == 4) xTEPATH0[3] = 1+taul[tt-1]-taul0;
-      if (budget_bal == 5) xTEPATH0[3] = 1+tauprof[tt-1]-tauprof0;
-      if (budget_bal == 6) xTEPATH0[3] = 1+cG[tt-1]-cG0;
-      
-      xTEPATH0[4] = ab[tt-1]/ab0;
-      
+      if (budget_bal == 1) xTEPATH0[3] = 1 + tauW[tt - 1] - tauW0
+      if (budget_bal == 2) xTEPATH0[3] = 1 + tauF[tt - 1] - tauF0
+      if (budget_bal == 3) xTEPATH0[3] = 1 + tauC[tt - 1] - tauC0
+      if (budget_bal == 4) xTEPATH0[3] = 1 + taul[tt - 1] - taul0
+      if (budget_bal == 5) xTEPATH0[3] = 1 + tauprof[tt - 1] - tauprof0
+      if (budget_bal == 6) xTEPATH0[3] = 1 + cG[tt - 1] - cG0
+
+      xTEPATH0[4] = ab[tt - 1] / ab0
+
     }
 
-    xout        = multiroot(TE,xTEPATH0,rtol=1e-12);    
-    if (abs(xout$estim.precis) > 1e-6) stop(paste("NEWTON METHOD DID NOT CONVERGE IN PERIOD ", tt, "\n",sep=""));
-    xTEPATH0 = xout$root;
-    tt     <<- tt + 1;
-    ncalls <<- ncalls + 1;
+    xout        = multiroot(TE, xTEPATH0, rtol = 1e-12)
+    if (abs(xout$estim.precis) > 1e-6) stop(paste("NEWTON METHOD DID NOT CONVERGE IN PERIOD ", tt, "\n", sep = ""))
+    xTEPATH0 = xout$root
+    tt     <<- tt + 1
+    ncalls <<- ncalls + 1
   }
-  
+
   # REVISED EXPECTATIONS
-  actual[,1]                        = t(V);
-  actual[,(1:(nag-fag+1))+1]        = t(Hv[fag:nag,]);
-  actual[,(nag-fag+3):numforesight] = t(Lambdav[fag:nag,]);
-  
-  return(actual);
+  actual[, 1]                        = t(V)
+  actual[, (1:(nag - fag + 1)) + 1]        = t(Hv[fag:nag, ])
+  actual[, (nag - fag + 3):numforesight] = t(Lambdav[fag:nag, ])
+
+  return(actual)
 }
 
 # GFT -- GENERALIZED FAIR TAYLOR ALGORITHM (Wilcoxen, P. 1990: "A fast algorithm for solving rational expectations models")
-gft <- function(f, guess, tstart, maxtry = 200L, tol = 1e-5, tune0 = 0.9, tune1 = 0.9, iprint = 1L) {
-  
-  tstart_loop = proc.time();
+gft = function(f, guess, tstart, maxtry = 200L, tol = 1e-5, tune0 = 0.9, tune1 = 0.9, iprint = 1L) {
 
-  trys   <<- 0;
-  ncalls <<- 0;
-  nvar   = ncol(guess);
-  nyrs   = nrow(guess);
-  convergence_reached = F;
-  
-  cat("\nRunning Generalized Fair Taylor (GFT) algorithm for Transition:\n\n");
+  tstart_loop = proc.time()
 
-  while (trys<maxtry) {
-      
-      tstart_iter = proc.time();
-    
-      trys <<- trys + 1;
-      
-      actual = f(guess,tstart,nyrs-1);
-    
-      mis     = max(abs(actual-guess));
-      mistol1 = log(mis/tol);
-      mistol2 = mis-tol;
-      
-      tstop_iter = proc.time();
-      
-      if (iprint >=1) cat(paste0("Iteration:  ", formatC(trys,width=3) ,"   Time: ",  format_dec(tstop_iter[3]-tstart_iter[3],3), " sec   log of err/tol: ", format_dec(mistol1,8),"\n"));
+  trys   <<- 0
+  ncalls <<- 0
+  nvar   = ncol(guess)
+  nyrs   = nrow(guess)
+  convergence_reached = F
+
+  cat("\nRunning Generalized Fair Taylor (GFT) algorithm for Transition:\n\n")
+
+  while (trys < maxtry) {
+
+      tstart_iter = proc.time()
+
+      trys <<- trys + 1
+
+      actual = f(guess, tstart, nyrs - 1)
+
+      mis     = max(abs(actual - guess))
+      mistol1 = log(mis / tol)
+      mistol2 = mis - tol
+
+      tstop_iter = proc.time()
+
+      if (iprint >= 1) cat(paste0("Iteration:  ", formatC(trys, width = 3), "   Time: ",  format_dec(tstop_iter[3] - tstart_iter[3], 3), " sec   log of err/tol: ", format_dec(mistol1, 8), "\n"))
 
       if (max(mistol2) < 0) {
-        convergence_reached = T;
-        cat(paste0(rep(" ",51),collapse=""),"Convergence!\n");
-        break;
+        convergence_reached = T
+        cat(paste0(rep(" ", 51), collapse = ""), "Convergence!\n")
+        break
       }
 
-      last_g = guess;
-          
-      i = nyrs-1;
+      last_g = guess
+
+      i = nyrs - 1
       while (i >= tstart) {
-        tmp1 = actual[i,]-tune1*jacob%*%as.matrix(last_g[i+1,]-guess[i+1,],nvar);
-        guess[i,] = tune0*tmp1 + (1-tune0)*last_g[i,];
-        i = i-1;
+        tmp1 = actual[i, ] - tune1 * jacob %*% as.matrix(last_g[i + 1, ] - guess[i + 1, ], nvar)
+        guess[i, ] = tune0 * tmp1 + (1 - tune0) * last_g[i, ]
+        i = i - 1
       }
 }
-  
+
   if (convergence_reached == F) {
-    actual = guess;
-    cat("\nIteration limit exceeded! no convergence!\n");
-    cat(paste("Maximum number of iterations  = ",maxtry,"\n",sep=""));
+    actual = guess
+    cat("\nIteration limit exceeded! no convergence!\n")
+    cat(paste("Maximum number of iterations  = ", maxtry, "\n", sep = ""))
   }
-  
-  lastguess <<- guess;
-    
-  cat(paste("\nNumber of intraperiod solutions = ",ncalls,"\n",sep=""));
-  cat(paste("Number of vector iterations     = ",trys,"\n",sep=""));
-  
-  tstop_loop <- proc.time();
-  
-  duration = tstop_loop[3]-tstart_loop[3];
-  cat(paste("\nCompution time           = " , duration, " sec\n",sep=""));
-  
-  return(actual);
+
+  lastguess <<- guess
+
+  cat(paste("\nNumber of intraperiod solutions = ", ncalls, "\n", sep = ""))
+  cat(paste("Number of vector iterations     = ", trys, "\n", sep = ""))
+
+  tstop_loop = proc.time()
+
+  duration = tstop_loop[3] - tstart_loop[3]
+  cat(paste("\nCompution time           = ", duration, " sec\n", sep = ""))
+
+  return(actual)
 }
 
 # computation of ISS Jacobians for GFT
-JACOB <- function(f, guess) {
-  
-  tstart = proc.time();
-  
-  cat("\nComputing Jacobian...");
+JACOB = function(f, guess) {
 
-  nvar  	 = ncol(guess);
-  jacob	   = zerosmat(nvar,nvar);
-  
-  actual	 = f(guess,1,2);
-  save_act = actual[1,];
-  step     = 0.001*guess[1,];
-  
+  tstart = proc.time()
+
+  cat("\nComputing Jacobian...")
+
+  nvar           = ncol(guess)
+  jacob    = zerosmat(nvar, nvar)
+
+  actual         = f(guess, 1, 2)
+  save_act = actual[1, ]
+  step     = 0.001 * guess[1, ]
+
   for (j in 1:nvar) {
-    guess[2,j]   = guess[2,j] + step[j];
-    actual 	     = f(guess,1,2);
-    guess[2,j]   = guess[2,j] - step[j];
-    jacob[,j]    = (actual[1,]-save_act)/step[j];
+    guess[2, j]   = guess[2, j] + step[j]
+    actual           = f(guess, 1, 2)
+    guess[2, j]   = guess[2, j] - step[j]
+    jacob[, j]    = (actual[1, ] - save_act) / step[j]
   }
-  
-  cat("OK!\n");
-  
-  tstop = proc.time();
-  
-  duration = toString(tstop[3]-tstart[3]);
-  cat(paste("Compution time           = " , duration, " sec\n",sep=""));
-  
-  tt <<- 1;
-  
-  return(jacob);
-}
 
+  cat("OK!\n")
+
+  tstop = proc.time()
+
+  duration = toString(tstop[3] - tstart[3])
+  cat(paste("Compution time           = ", duration, " sec\n", sep = ""))
+
+  tt <<- 1
+
+  return(jacob)
+}
--- TE.R
+++ TE.R
@@ -1,106 +1,106 @@
-TE <- function(inputs) {
-  
-  resid = zeroscol(4);
-  
-  r[tt]     <<- r0*inputs[1];
-  w[tt]     <<- w0*inputs[2];
-  
-  if (budget_bal == 1) tauWv[,tt]  <<- tauWv0+(inputs[3]-1);
-  if (budget_bal == 2) tauF[tt]    <<- tauF0+(inputs[3]-1);
-  if (budget_bal == 3) tauC[tt]    <<- tauC0+(inputs[3]-1);
-  if (budget_bal == 4) taul[tt]    <<- taul0+(inputs[3]-1);
-  if (budget_bal == 5) tauprof[tt] <<- tauprof0+(inputs[3]-1);
-  if (budget_bal == 6) cGV[,tt]    <<- cGV0+(inputs[3]-1);
-  
-  # accidental bequest
-  ab[tt]          <<- ab0*inputs[4];
-  Nc[tt]          <<- sum(Nv[1:(fag-1),tt]);
-  abv[fag:nag,tt] <<- ab[tt]/(N[tt]-Nc[tt])*onescol(nag-fag+1);
+TE = function(inputs) {
 
-  pc[tt]          <<- 1+tauC[tt];
-  CG[tt]          <<- sum(cGv[,tt]*Nv[,tt]);
-  cG[tt]          <<- CG[tt]/N[tt];
-  
+  resid = zeroscol(4)
+
+  r[tt]     <<- r0 * inputs[1]
+  w[tt]     <<- w0 * inputs[2]
+
+  if (budget_bal == 1) tauWv[, tt]  <<- tauWv0 + (inputs[3] - 1)
+  if (budget_bal == 2) tauF[tt]    <<- tauF0 + (inputs[3] - 1)
+  if (budget_bal == 3) tauC[tt]    <<- tauC0 + (inputs[3] - 1)
+  if (budget_bal == 4) taul[tt]    <<- taul0 + (inputs[3] - 1)
+  if (budget_bal == 5) tauprof[tt] <<- tauprof0 + (inputs[3] - 1)
+  if (budget_bal == 6) cGV[, tt]    <<- cGV0 + (inputs[3] - 1)
+
+  # accidental bequest
+  ab[tt]          <<- ab0 * inputs[4]
+  Nc[tt]          <<- sum(Nv[1:(fag - 1), tt])
+  abv[fag:nag, tt] <<- ab[tt] / (N[tt] - Nc[tt]) * onescol(nag - fag + 1)
+
+  pc[tt]          <<- 1 + tauC[tt]
+  CG[tt]          <<- sum(cGv[, tt] * Nv[, tt])
+  cG[tt]          <<- CG[tt] / N[tt]
+
   # RETIREMENT
-  Nw[tt]          <<- sum(notretv[,tt]*Nv[,tt]); 		     # total nb workers
-  Nr[tt] 	        <<- sum((1-notretv[,tt])*Nv[,tt]); 	   # nb retirees
-  
+  Nw[tt]          <<- sum(notretv[, tt] * Nv[, tt])                      # total nb workers
+  Nr[tt]                <<- sum((1 - notretv[, tt]) * Nv[, tt])          # nb retirees
+
   # HOURS SUPPLY
-  ellv[fag:nag,tt]     <<- ((w[tt]*(1-tauWv[fag:nag,tt])*thetav[fag:nag,tt]/pc[tt])/parlv0[fag:nag])^sigL;
-  dis_totv[fag:nag,tt] <<- (sigL/(1+sigL))*parlv0[fag:nag]*ellv[fag:nag,tt]^((1+sigL)/sigL)-parlv1[fag:nag];
-  yv[fag:nag,tt]       <<- notretv[fag:nag,tt]*(w[tt]*(1-tauWv[fag:nag,tt])*ellv[fag:nag,tt]*thetav[fag:nag,tt])+(1-notretv[fag:nag,tt])*(1-tauWv[fag:nag,tt])*pv[fag:nag,tt]-taulv[fag:nag,tt];
+  ellv[fag:nag, tt]     <<- ((w[tt] * (1 - tauWv[fag:nag, tt]) * thetav[fag:nag, tt] / pc[tt]) / parlv0[fag:nag])^sigL
+  dis_totv[fag:nag, tt] <<- (sigL / (1 + sigL)) * parlv0[fag:nag] * ellv[fag:nag, tt]^((1 + sigL) / sigL) - parlv1[fag:nag]
+  yv[fag:nag, tt]       <<- notretv[fag:nag, tt] * (w[tt] * (1 - tauWv[fag:nag, tt]) * ellv[fag:nag, tt] * thetav[fag:nag, tt]) + (1 - notretv[fag:nag, tt]) * (1 - tauWv[fag:nag, tt]) * pv[fag:nag, tt] - taulv[fag:nag, tt]
 
   # TOTAL LABOR SUPPLY
-  LS[tt]    <<- sum(notretv[,tt]*ellv[,tt]*thetav[,tt]*Nv[,tt]);
-  
+  LS[tt]    <<- sum(notretv[, tt] * ellv[, tt] * thetav[, tt] * Nv[, tt])
+
   # PRODUCTION
-  Y[tt]        <<- TFP[tt]*(K[tt]^alpha)*(LS[tt]^(1-alpha));
-  LD[tt]       <<- ((1-alpha)*TFP[tt]/((1+tauF[tt])*w[tt]))^(1/alpha)*K[tt];
-  
+  Y[tt]        <<- TFP[tt] * (K[tt]^alpha) * (LS[tt]^(1 - alpha))
+  LD[tt]       <<- ((1 - alpha) * TFP[tt] / ((1 + tauF[tt]) * w[tt]))^(1 / alpha) * K[tt]
+
   # INVESTMENT
-  uck[tt+1] <<- (r[tt]+delta*(1-tauprof[tt+1]))/(1-tauprof[tt+1]);
-  qTob[tt]  <<- (1-tauprof[tt])*alpha*TFP[tt]*(K[tt]/LD[tt])^(alpha-1)+tauprof[tt]*delta+(1-delta);
-  Inv[tt]   <<- V[tt+1]/(1+r[tt])-(1-delta)*K[tt];
-  K[tt+1]   <<- Inv[tt]+(1-delta)*K[tt];
+  uck[tt + 1] <<- (r[tt] + delta * (1 - tauprof[tt + 1])) / (1 - tauprof[tt + 1])
+  qTob[tt]  <<- (1 - tauprof[tt]) * alpha * TFP[tt] * (K[tt] / LD[tt])^(alpha - 1) + tauprof[tt] * delta + (1 - delta)
+  Inv[tt]   <<- V[tt + 1] / (1 + r[tt]) - (1 - delta) * K[tt]
+  K[tt + 1]   <<- Inv[tt] + (1 - delta) * K[tt]
 
   # FIRM VALUE
-  TaxF[tt]  <<- tauprof[tt]*(Y[tt]-(1+tauF[tt])*w[tt]*LD[tt]-delta*K[tt]);
-  Div[tt]   <<- Y[tt]-(1+tauF[tt])*w[tt]*LD[tt]-Inv[tt]-TaxF[tt];
-  V[tt]     <<- Div[tt]+V[tt+1]/(1+r[tt]);
-  
+  TaxF[tt]  <<- tauprof[tt] * (Y[tt] - (1 + tauF[tt]) * w[tt] * LD[tt] - delta * K[tt])
+  Div[tt]   <<- Y[tt] - (1 + tauF[tt]) * w[tt] * LD[tt] - Inv[tt] - TaxF[tt]
+  V[tt]     <<- Div[tt] + V[tt + 1] / (1 + r[tt])
+
   # CONSUMPTION
-  Lambdav[nag,tt]       <<- pc[tt]^(1-sigma);
-  Lambdav[fag:(nag-1),tt] <<- pc[tt]^(1-sigma)+(1/(1+rho)*gamv[fag:(nag-1),tt])^sigma*(1+r[tt])^(sigma-1)*Lambdav[(fag+1):nag,tt+1];
-  Omegav[,tt]           <<- Lambdav[,tt]/(pc[tt]^(1-sigma));
-  
-  Hv[nag,tt]           <<- yv[nag,tt]-dis_totv[nag,tt]*pc[tt]+ivv[nag,tt]+abv[nag,tt];
-  Hv[fag:(nag-1),tt]   <<- yv[fag:(nag-1),tt]-dis_totv[fag:(nag-1),tt]*pc[tt]+ivv[fag:(nag-1),tt]+abv[fag:(nag-1),tt]+Hv[(fag+1):nag,tt+1]/(1+r[tt]);
-  
-  A[tt]          <<- V[tt]+DG[tt];
-  Av[,tt]        <<- shareAv[,tt]/Nv[,tt]*A[tt];
-  Qv[fag:nag,tt]        <<- (Av[fag:nag,tt]+Hv[fag:nag,tt])/(pc[tt]*Omegav[fag:nag,tt]);
-  Consv[fag:nag,tt]     <<- Qv[fag:nag,tt]+dis_totv[fag:nag,tt];
-  
+  Lambdav[nag, tt]       <<- pc[tt]^(1 - sigma)
+  Lambdav[fag:(nag - 1), tt] <<- pc[tt]^(1 - sigma) + (1 / (1 + rho) * gamv[fag:(nag - 1), tt])^sigma * (1 + r[tt])^(sigma - 1) * Lambdav[(fag + 1):nag, tt + 1]
+  Omegav[, tt]           <<- Lambdav[, tt] / (pc[tt]^(1 - sigma))
+
+  Hv[nag, tt]           <<- yv[nag, tt] - dis_totv[nag, tt] * pc[tt] + ivv[nag, tt] + abv[nag, tt]
+  Hv[fag:(nag - 1), tt]   <<- yv[fag:(nag - 1), tt] - dis_totv[fag:(nag - 1), tt] * pc[tt] + ivv[fag:(nag - 1), tt] + abv[fag:(nag - 1), tt] + Hv[(fag + 1):nag, tt + 1] / (1 + r[tt])
+
+  A[tt]          <<- V[tt] + DG[tt]
+  Av[, tt]        <<- shareAv[, tt] / Nv[, tt] * A[tt]
+  Qv[fag:nag, tt]        <<- (Av[fag:nag, tt] + Hv[fag:nag, tt]) / (pc[tt] * Omegav[fag:nag, tt])
+  Consv[fag:nag, tt]     <<- Qv[fag:nag, tt] + dis_totv[fag:nag, tt]
+
   # assets and consumption
   # solve forward in age
-  Av[fag,tt+1]         <<- 0;
-  Av[(fag+1):nag,tt+1] <<- (1+r[tt])*(Av[fag:(nag-1),tt]+yv[fag:(nag-1),tt]+ivv[fag:(nag-1),tt]+abv[fag:(nag-1),tt]-pc[tt]*Consv[fag:(nag-1),tt]);
-  
-  Savv[,tt]      <<- Av[,tt]+yv[,tt]+ivv[,tt]+abv[,tt]-pc[tt]*Consv[,tt];
+  Av[fag, tt + 1]         <<- 0
+  Av[(fag + 1):nag, tt + 1] <<- (1 + r[tt]) * (Av[fag:(nag - 1), tt] + yv[fag:(nag - 1), tt] + ivv[fag:(nag - 1), tt] + abv[fag:(nag - 1), tt] - pc[tt] * Consv[fag:(nag - 1), tt])
+
+  Savv[, tt]      <<- Av[, tt] + yv[, tt] + ivv[, tt] + abv[, tt] - pc[tt] * Consv[, tt]
 
   # AGGREGATION
-  A[tt+1]        <<- sum(Av[,tt+1]*Nv[,tt+1]);
-  shareAv[,tt+1] <<- (Av[,tt+1]*Nv[,tt+1])/A[tt+1];
-  Cons[tt]       <<- sum(Consv[,tt]*Nv[,tt]);
-  P[tt]          <<- sum((1-notretv[,tt])*pv[,tt]*Nv[,tt]);                # expend pensions
-  tauW[tt]       <<- sum(tauWv[,tt]*notretv[,tt]*ellv[,tt]*thetav[,tt]*Nv[,tt])/LS[tt];
-  TaxP           = sum((1-notretv[,tt])*tauWv[,tt]*pv[,tt]*Nv[,tt]);
-  Taxl           = sum(taulv[,tt]*Nv[,tt]);
+  A[tt + 1]        <<- sum(Av[, tt + 1] * Nv[, tt + 1])
+  shareAv[, tt + 1] <<- (Av[, tt + 1] * Nv[, tt + 1]) / A[tt + 1]
+  Cons[tt]       <<- sum(Consv[, tt] * Nv[, tt])
+  P[tt]          <<- sum((1 - notretv[, tt]) * pv[, tt] * Nv[, tt])                # expend pensions
+  tauW[tt]       <<- sum(tauWv[, tt] * notretv[, tt] * ellv[, tt] * thetav[, tt] * Nv[, tt]) / LS[tt]
+  TaxP           = sum((1 - notretv[, tt]) * tauWv[, tt] * pv[, tt] * Nv[, tt])
+  Taxl           = sum(taulv[, tt] * Nv[, tt])
 
   # GOVERNMENT
-  PB[tt]    <<- DG[tt]-DG[tt+1]/(1+r[tt]);
-  Exp[tt]   <<- CG[tt]+P[tt];
-  Rev[tt]   <<- TaxF[tt]+(tauF[tt]*LD[tt]+tauW[tt]*LS[tt])*w[tt]+tauC[tt]*Cons[tt]+TaxP+Taxl;
-  
+  PB[tt]    <<- DG[tt] - DG[tt + 1] / (1 + r[tt])
+  Exp[tt]   <<- CG[tt] + P[tt]
+  Rev[tt]   <<- TaxF[tt] + (tauF[tt] * LD[tt] + tauW[tt] * LS[tt]) * w[tt] + tauC[tt] * Cons[tt] + TaxP + Taxl
+
   # EXCESS DEMANDS
-  edy[tt]   <<- Inv[tt]+Cons[tt]+CG[tt]-Y[tt];
-  edl[tt]   <<- LD[tt]-LS[tt];
-  edg[tt]   <<- Rev[tt]-Exp[tt]-PB[tt];
-  eda[tt]   <<- DG[tt]+V[tt]-A[tt];
-  eda[tt+1] <<- DG[tt+1]+V[tt+1]-A[tt+1];
-  ediv[tt]  <<- -sum(ivv[,tt]*Nv[,tt]);
-  edab[tt]  <<- sum((1-gamv[,tt])*Savv[,tt]*Nv[,tt])-ab[tt];
-  
+  edy[tt]   <<- Inv[tt] + Cons[tt] + CG[tt] - Y[tt]
+  edl[tt]   <<- LD[tt] - LS[tt]
+  edg[tt]   <<- Rev[tt] - Exp[tt] - PB[tt]
+  eda[tt]   <<- DG[tt] + V[tt] - A[tt]
+  eda[tt + 1] <<- DG[tt + 1] + V[tt + 1] - A[tt + 1]
+  ediv[tt]  <<- -sum(ivv[, tt] * Nv[, tt])
+  edab[tt]  <<- sum((1 - gamv[, tt]) * Savv[, tt] * Nv[, tt]) - ab[tt]
+
   # WALRAS LAW
-  edw[tt]  <<- edy[tt]+w[tt]*edl[tt]+edg[tt]+ediv[tt]+edab[tt]+eda[tt]-eda[tt+1]/(1+r[tt]);
-  #if (abs(edw[tt])> 1e-8) stop("Error in TE: Walras Law does not hold!\n");
-  
-  #resid[1] = edy[tt];
-  resid[1] = eda[tt+1]; # equivalent to eda[tt+1]
-  resid[2] = edl[tt];
-  resid[3] = edg[tt];
-  resid[4] = edab[tt];
-  
-  return(resid);
+  edw[tt]  <<- edy[tt] + w[tt] * edl[tt] + edg[tt] + ediv[tt] + edab[tt] + eda[tt] - eda[tt + 1] / (1 + r[tt])
+  # if (abs(edw[tt])> 1e-8) stop("Error in TE: Walras Law does not hold!\n")
+
+  # resid[1] = edy[tt]
+  resid[1] = eda[tt + 1] # equivalent to eda[tt+1]
+  resid[2] = edl[tt]
+  resid[3] = edg[tt]
+  resid[4] = edab[tt]
+
+  return(resid)
 
 }
